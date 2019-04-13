265d1469bc22bd0ef36f0926b822b94b5b7067bf
Deepak Ramachandran <42649107+ProgrammingMuffin@users.noreply.github.com>
2019-04-13 15:53:33 +0530

    Initial commit

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<link href="stylesheets/dashboard.css" type="text/CSS" rel="stylesheet">
+</head>
+<body>
+	<h2>Click on the buttons inside the tabbed menu: </h2>
+	<div class="menu" >
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button1" class="button" >Add Cases</div></a>
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button2" class="button" >Add Officers</div></a>
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button3" class="button" >Predict Results</div></a>
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button4" class="button" >Add Results</div></a>
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button5" class="button" >View Evidence</div></a>
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button6" class="button" >View Case History</div></a>
+		<a href="ENTER_FILE_NAME_HERE" ><div id="button7" class="button" >Log Out</div></a>
+	</div>
+</body>
+</html>
